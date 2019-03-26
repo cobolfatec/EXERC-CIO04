ebee6f861b32a92337bfdadb038b12985b976816
Alvaro Pereira do Nascimento <33030108+AlvaroPereiradoNascimento@users.noreply.github.com>
2019-03-26 11:41:42 -0300

    Efetuado implementação
    Efetuado implementação de sysout e outras variaveis

--- CGPRG04M.cob
+++ CGPRG04M.cob
@@ -35,12 +35,25 @@
 
       *-----> VARIAVEIS AUXILIARES UTILIZADA NO PROCESSAMENTO
        01  WS-AREA-AUX.
-           05  WS-FIM                 PIC X(01).
            05  AS-mediag-IN           PIC 9(02)V99.
+           05  WS-FIM                 PIC 9(01).
+           O5  WS-CTLIDO              PIC 9(02).
+           05  WS-MEDIA               PIC 9(02)V99.
            05  AS-SEXM                PIC X(02).
+           05  AS-ABMED               PIC 99.
 
       *-----> ENTRADA - DADOS VIA SYSIN (NO JCL DE EXECUCAO)
-
+      
+      01  WS-REG-SYSIN.
+           05 WS-NUM              PIC 9(04).
+           05 WS-NOM              PIC X(20).
+           05 WS-SEX              PIC X(01).
+           05 WS-IDA              PIC 9(02).
+           05 WS-CUR              PIC X(12).
+           05 WS-NOT1-IN          PIC Z9V99.
+           05 WS-NOT2-IN          PIC Z9V99.
+           WS-MED                 PIC Z9V99.
+      
        01  WS-REG-SYSOUT.
            05 WS-NUM              PIC 9(04).
            FILLER                 PIC X(01) VALUES SPACES.
@@ -100,11 +113,19 @@
 
            COMPUTE WS-MED = (WS-NOTAA1 + WS-NOTAA2) / 2
            DISPLAY WS-REG-SYSOUT
-           DISPLAY WS-MEDIA
-           ADD 1  TO AS-REGISTRO
+           DISPLAY WS-MED
+      *    VERIFICADOR SE SEXO E MASCULINO    *
            IF   AS-SEXO-IN  = 'M'
-             ADD 1 TO AS-SEXM
+              ADD 1  TO WS-SEX
+           END IF.
+      *    VERIFICAR SE ALUNO ESTA ABAIXO DA MEDIA   *
+           IF   WS-MED < 6
+              ADD 1  TO AS-ABMED
+           END IF.
+      *    ADICIONA MEDIA DO ALUNOA A MEDIA GERAL    *
+           ADD WS-MED TO AS-mediag-IN   
            PERFORM 025-LER-SYSIN
+             
            .
       *--------------------------------------------------------------*
       *    PROCEDIMENTOS FINAIS
